ca5fe609f6befe83a2659a2c4e09e89960e74be7
Aman Grewal <amangrewal2002@gmail.com>
2025-05-02 21:14:00 -0700

    add matching pennies opponent test

---
+++ tests/test_games.py
@@ -0,0 +1,14 @@
+import numpy as np
+from games import rock_paper_scissors, matching_pennies
+
+
+def test_rps_shape():
+    A = rock_paper_scissors()
+    assert A.shape == (3, 3)
+    assert np.allclose(A + A.T, 0)
+
+
+def test_pennies_shape():
+    A = matching_pennies()
+    assert A.shape == (2, 2)
+    assert np.allclose(A + A.T, 0)
